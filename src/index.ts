e11f3c877a9a254a660ffe97c5121935dad78967
Nicolas Rivière <nicolas.riviere@cetrafact.fr>
2021-07-30 11:35:23 +0200

    Export class-validation functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export {Application, AppOptions} from "./application"
 export {Orm} from "./orm";
 export { v4 } from 'uuid';
-export {Get, JsonController} from "routing-controllers";
+export {JsonController, Get, Post, Delete, Body} from "routing-controllers";
 export {Service} from "typedi";
-export {Query, Resolver, ObjectType, Field} from "type-graphql"
+export {Query, Resolver, ObjectType, Field} from "type-graphql";
 
 // Mikro ORM
 export {
@@ -17,7 +17,24 @@ export {
     EntityRepository,
     Subscriber,
     EventSubscriber,
-    EventArgs
+    EventArgs,
+    BeforeCreate
 } from "@mikro-orm/core"
 
 export { Migration } from '@mikro-orm/migrations';
+
+// Class-validator
+export {
+    IsDefined,
+    IsOptional,
+    IsString,
+    IsNumber,
+    IsInt,
+    IsEmail,
+    IsUUID,
+    IsDate,
+    IsDateString,
+    IsPhoneNumber,
+    IsEmpty
+} from "class-validator";
+
